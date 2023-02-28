90bb7954ee6e6ee3da5afff317ad369f9bf91856
Sandro de Gruijter <sandrodegruijter@gmail.com>
2023-02-28 15:34:42 +0100

    Added new blue flag

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Component, createEffect, For } from 'solid-js';
 import drivers from './assets/drivers.json';
 import Driver from './Driver/Driver';
 import './App.css';
+import Flag from './Flag/Flag';
 
 const App: Component = () => {
     let gap = 0;
@@ -14,58 +15,59 @@ const App: Component = () => {
     });
 
     return (
-        <div class='tower-container'>
-            <div class='blurry-background'>
-                <div class='main-background'>
-                    <div class='header'>
-                        <div class='title'></div>
-                        <div class='race-info'>
-                            <div class='laps'>
-                                <span>
-                                    LAP{' '}
-                                    <span
-                                        style={{
-                                            color: 'white',
-                                            'font-size': '1.5em',
-                                        }}
-                                    >
-                                        3
+        <>
+            <div class='tower-container'>
+                <div class='blurry-background'>
+                    <div class='main-background'>
+                        <div class='header'>
+                            <div class='title'></div>
+                            <div class='race-info'>
+                                <div class='laps'>
+                                    <span>
+                                        LAP{' '}
+                                        <span
+                                            style={{
+                                                color: 'white',
+                                                'font-size': '1.5em',
+                                            }}
+                                        >
+                                            3
+                                        </span>
+                                        /57
                                     </span>
-                                    /57
-                                </span>
-                            </div>
-                            <div class='checkpoints'>
-                                <span>
-                                    CP{' '}
-                                    <span
-                                        style={{
-                                            color: 'white',
-                                            'font-size': '1.5em',
-                                        }}
-                                    >
-                                        8
+                                </div>
+                                <div class='checkpoints'>
+                                    <span>
+                                        CP{' '}
+                                        <span
+                                            style={{
+                                                color: 'white',
+                                                'font-size': '1.5em',
+                                            }}
+                                        >
+                                            8
+                                        </span>
+                                        /46
                                     </span>
-                                    /46
-                                </span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class='drivers'>
-                        <For each={drivers.drivers}>
-                            {(driver, i) => (
-                                i() !== 0 ? (gap += Math.random() * 3) : 0,
-                                (
-                                    <Driver
-                                        brand={driver.brand}
-                                        gap={gap.toFixed(3)}
-                                        name={driver.name}
-                                        position={driver.position}
-                                        player={'player' in driver}
-                                    />
-                                )
-                            )}
-                        </For>
-                        {/* {drivers.drivers.map((driver, index) => {
+                        <div class='drivers'>
+                            <For each={drivers.drivers}>
+                                {(driver, i) => (
+                                    i() !== 0 ? (gap += Math.random() * 3) : 0,
+                                    (
+                                        <Driver
+                                            brand={driver.brand}
+                                            gap={gap.toFixed(3)}
+                                            name={driver.name}
+                                            position={driver.position}
+                                            player={'player' in driver}
+                                        />
+                                    )
+                                )}
+                            </For>
+                            {/* {drivers.drivers.map((driver, index) => {
                             const output = (
                                 <Driver
                                     brand={driver.brand}
@@ -81,10 +83,12 @@ const App: Component = () => {
 
                             return output;
                         })} */}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <Flag />
+        </>
     );
 };
 
---
+++ src/Flag/Flag.tsx
@@ -0,0 +1,21 @@
+import './Flag.css';
+
+function Flag() {
+    return (
+        <div class='flag-container'>
+            <div class='flag-blurry-background'>
+                <div class='flag'>
+                    <div class='message-container'>
+                        <div class='logo'></div>
+                        <div class='message'>BLUE FLAG</div>
+                    </div>
+                    <div class='explanation'>
+                        Faster car(s) approaching behind
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Flag;
