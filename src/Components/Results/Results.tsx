c70a1cec56f25c062bb6ca3880a1d40b20cfaac9
Sandro de Gruijter <sandrodegruijter@gmail.com>
2023-03-01 15:52:14 +0100

    Work on the results page

--- src/Components/Results/Results.tsx
+++ src/Components/Results/Results.tsx
@@ -1,7 +1,8 @@
 import { For, Show } from 'solid-js';
 import classes from './Results.module.css';
-import drivers from '../../assets/drivers.json';
-import Driver from '../Driver/Driver';
+import data from '../../assets/drivers.json';
+import TowerDriver from '../TowerDriver/TowerDriver';
+import ResultsDriver from '../ResultsDriver/ResultsDriver';
 
 interface IResults {
     visible: boolean;
@@ -15,12 +16,15 @@ function Results(props: IResults) {
                     <div class={classes.mainContent}>
                         <div class={classes.header}>
                             <div class={classes.raceName}>
-                                FORMULA 1 GULF AIR BAHRAIN GRAND PRIX 2022
+                                Blaine County 24h
                             </div>
                             <div class={classes.label}>
                                 <div class={classes.position}>#</div>
-                                <div class={classes.multi}># in Class</div>
+                                {data.multiclass && (
+                                    <div class={classes.multi}># in Class</div>
+                                )}
                                 <div class={classes.name}>Name</div>
+                                <div class={classes.brand}>Brand</div>
                                 <div class={classes.car}>Car</div>
                                 <div class={classes.bestLap}>Fastest Lap</div>
                                 <div class={classes.fps}>FPS</div>
@@ -28,14 +32,18 @@ function Results(props: IResults) {
                             </div>
                         </div>
                         <div class={classes.drivers}>
-                            <For each={drivers.drivers}>
+                            <For each={data.drivers}>
                                 {(driver, i) => (
-                                    <Driver
+                                    <ResultsDriver
                                         brand={driver.brand}
-                                        gap={driver.gap.toFixed(3)}
                                         name={driver.name}
                                         player={'player' in driver}
                                         position={driver.position}
+                                        car={driver.car}
+                                        fastestLap={driver.fastestLap}
+                                        fps={driver.fps}
+                                        ping={driver.ping}
+                                        positionInClass={driver.positionInClass}
                                     />
                                 )}
                             </For>
--- src/Components/ResultsDriver/ResultsDriver.tsx
+++ src/Components/ResultsDriver/ResultsDriver.tsx
@@ -1,7 +1,56 @@
+import { Show } from 'solid-js';
 import classes from './ResultsDriver.module.css';
+import data from '../../assets/drivers.json';
 
-function ResultsDriver() {
-    return <div>ResultsDriver</div>;
+interface IResultsDriver {
+    position: number;
+    positionInClass: number;
+    name: string;
+    car: string;
+    fastestLap: number;
+    fps: number;
+    ping: number;
+    brand: string;
+    player: boolean;
+}
+
+function ResultsDriver({
+    position,
+    positionInClass,
+    name,
+    car,
+    fastestLap,
+    fps,
+    ping,
+    brand,
+    player,
+}: IResultsDriver) {
+    const date = new Date(fastestLap * 1000);
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const millis = date.getMilliseconds();
+
+    return (
+        <div class={classes.resultsDriverContainer}>
+            <div class={classes.position}>{position}</div>
+            {data.multiclass && (
+                <div class={classes.positionInClass}>{positionInClass}</div>
+            )}
+            <div class={classes.name}>{name}</div>
+            <div
+                class={`${classes.brand}`}
+                style={{
+                    'background-image': `url('./src/assets/images/${brand}.webp')`,
+                }}
+            ></div>
+            <div class={classes.car}>{car}</div>
+            <div class={classes.fastestLap}>{`${minutes}:${String(
+                seconds
+            ).padStart(2, '0')}.${String(millis).padStart(3, '0')}`}</div>
+            <div class={classes.fps}>{fps}</div>
+            <div class={classes.ping}>{ping}</div>
+        </div>
+    );
 }
 
 export default ResultsDriver;
